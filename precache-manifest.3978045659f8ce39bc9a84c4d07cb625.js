42308574f24f8c9f63b98859aaa96f3276137edb
DivyaBasatwar <divyabasatwar02@gmail.com>
2021-04-08 15:10:36 +0530

    Updates

---
+++ precache-manifest.3978045659f8ce39bc9a84c4d07cb625.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2ce269fb9117a4b7930bff3732a517",
+    "url": "/FoodDeliveryAppInReact/index.html"
+  },
+  {
+    "revision": "e4cf938027f7561437a0",
+    "url": "/FoodDeliveryAppInReact/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "f61b11ba222f656fb24b",
+    "url": "/FoodDeliveryAppInReact/static/css/main.9e6ee501.chunk.css"
+  },
+  {
+    "revision": "e4cf938027f7561437a0",
+    "url": "/FoodDeliveryAppInReact/static/js/2.b7b4a2d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FoodDeliveryAppInReact/static/js/2.b7b4a2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61b11ba222f656fb24b",
+    "url": "/FoodDeliveryAppInReact/static/js/main.81a483a9.chunk.js"
+  },
+  {
+    "revision": "5da254e323d1c5d70717",
+    "url": "/FoodDeliveryAppInReact/static/js/runtime-main.486c79ab.js"
+  }
+]);
